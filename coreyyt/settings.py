949259e64090cd2837a8526c736e12184d6e70ea
Keyur Kumbhare <39528075+keyurkumbhare@users.noreply.github.com>
2020-05-25 22:56:00 +0530

    second try

--- coreyyt/settings.py
+++ coreyyt/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k(!hd48i=xtjtv8p+wh_=t9ljwa)3j+3!pkn($p=gn@r9^qjy_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
